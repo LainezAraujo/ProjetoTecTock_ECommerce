fa9307ffbc642e97f928751ca2e2584ce1f9ed2e
Vagner Garcia <garciavagnata.vg@gmail.com>
2018-12-08 17:33:39 -0300

    Botão logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,31 +7,14 @@
 
 </head>
 <body>
-@guest
-    <li><a href="{{ route('login') }}">Login</a></li>
-    <li><a href="{{ route('register') }}">Register</a></li>
-@else
-    <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"
-           aria-haspopup="true" v-pre>
-            {{ Auth::user()->name }} <span class="caret"></span>
-        </a>
-
-        <ul class="dropdown-menu">
-            <li>
-                <a href="{{ route('logout') }}"
-                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                    Logout
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    {{ csrf_field() }}
-                </form>
-            </li>
-        </ul>
-    </li>
-@endguest
+<div class="badge">
+    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        <button class="btn btn-secondary">Logout</button>
+    </a>
+</div>
+<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    {{ csrf_field() }}
+</form>
 <div class="container">
     @yield ('content')
 </div>
