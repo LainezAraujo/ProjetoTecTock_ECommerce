c51719dda353dd9c32bf098084f61e09f6d54f97
Lainez <lainezlsaraujo@gmail.com>
2018-12-08 17:26:02 -0300

    Tela inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,40 @@
-@extends('layouts.app')
+@extends('layouts.default')
 
+@section('title')
+    TecToc
+@endsection
 @section('content')
-<div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src={{asset("img\userlogo.png")}} alt="Card image logo">
+                <div class="card-body">
+                    <h5 class="card-title">Pessoa</h5>
+                    <p class="card-text">Listagem e novos cadastros.</p>
+                    <a href="#" class="btn  btn-secondary">Entrar</a>
+                </div>
+            </div>
+        </div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src={{asset("img\pclogo.png")}} alt="Card image logo">
+                <div class="card-body">
+                    <h5 class="card-title">Equipamento</h5>
+                    <p class="card-text">Listagem de equipamentos.</p>
+                    <a href="#" class="btn btn-secondary">Entrar</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src={{asset("img\documentlogo.png")}} alt="Card image logo">
+                <div class="card-body">
+                    <h5 class="card-title">Relatório</h5>
+                    <p class="card-text">Gerar relatórios.</p>
+                    <a href="#" class="btn btn-secondary">Entrar</a>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
